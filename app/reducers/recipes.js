98a7d3469f285d81d6d811985f0c2acd2f5726b6
Thiago Garcia <thiago.garcia@trt18.jus.br>
2017-08-23 14:06:51 -0300

    comentários melhorados

--- app/reducers/recipes.js
+++ app/reducers/recipes.js
@@ -4,8 +4,10 @@ import * as types from '../actions/types';
 //createReducer recebe array[funções handler] p cada type de action
 //(isso evita switch stsm) e o state inicial, e retornam 1 reducer.
 
-//reducer é 1 function que recebe um initial-state e um action
-//e faze alguma lógica de negócio dependendo da action.type.
+//createReducer(...) recebe um initial-state e uma outra
+//function com handlers que fazem lógicas de negócio
+//dependendo da action.type
+//e retorna o reduzer, que é uma function que recebe(inicialState,action)
 
 //cada const abaixo é uma variável na state-store... por exemplo,
 //searchedRecipes tem o state sendo um objeto com receitas
